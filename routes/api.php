d60aac47dd77fa2f9c1694edb6a72dcbcbd85cca
Mirza Tahir Baig <tahir.baig@ozoneddigital.com>
2025-04-17 17:47:12 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/todo',function(){
     return json_encode(['Hello World']);
+});
+
+Route::post('/todo',function (Request $request){
+    dd($request);
 });
