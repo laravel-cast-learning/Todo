8951b228e4f404bd4ddcd08d6164e58bb25fb080
Mirza Tahir Baig <tahir.baig@ozoneddigital.com>
2025-04-22 14:32:50 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,32 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
+use App\Models\Todo;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/todo',function(){
-    return json_encode(['Hello World']);
+    return response()->json([
+        'todos' => Todo::all()
+    ]);
 });
 
 Route::post('/todo',function (Request $request){
-    dd($request);
+    $validated = $request->validate([
+        'title' => 'required',
+        'description' => 'required'
+    ]);
+    return response()->json([Todo::create($validated)]);
+});
+
+Route::post('/todo/{id}',function ($id){
+    $todo = Todo::find($id);
+    $todo->completed = true;
+    $todo->save();
+    return response()->json([$todo]);
+});
+
+Route::delete('/todo/{id}',function ($id){
+    $todo = Todo::find($id);
+    $todo->delete();
+    return response()->json([$todo]);
 });
